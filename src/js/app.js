8ffbeaba96634529ad5c370fded882d0b566e74a
Sofia Perez <sofiaperez@MacBook-Air-de-Sofia.local>
2021-08-11 21:31:11 -0500

    added carousel logic into the carousel.js file

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 import '../scss/main.scss';
+import { initCarousel } from './carousel';
 
+initCarousel('#carousel');
 console.log("Hello, Webpack!");
+
---
+++ src/js/carousel.js
@@ -0,0 +1,33 @@
+export const initCarousel = (selector) => {
+    const carousel = document.querySelector(".carousel")
+    const slider = carousel.querySelector(".slider");
+    let currentIndex = 0;
+
+    // auto-play carousel logic
+    setInterval(() => {
+        dots[currentIndex].classList.remove("active")
+        currentIndex = currentIndex === 3 ? 0 : currentIndex + 1;
+        slider.style.transform = `translateX(${-100*currentIndex}%)`;
+        slider.style.transition = '.7s';
+        dots[currentIndex].classList.add("active")
+    }, 8000);
+
+    // dots navigation carousel logic
+    const dotsContainer = carousel.querySelector(".dots-list");
+    const dots = Array.from(dotsContainer.children);
+
+    const onClickDot = (event) => {
+        dots[currentIndex].classList.remove("active")
+        const targetDot = event.target;
+        const targetIndex = dots.findIndex(dot => dot === targetDot);
+        currentIndex = targetIndex;
+        slider.style.transform = `translateX(${-100*currentIndex}%)`;
+        slider.style.transition = '.7s';
+        dots[currentIndex].classList.add("active")
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("click", onClickDot);
+    };
+
+    };
