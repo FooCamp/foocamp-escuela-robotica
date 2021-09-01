b1d8760f526dce5d5ab62fb3b2a0886fecb7312f
sofiaperezm <sofi.prz.m@gmail.com>
2021-09-01 13:53:20 -0500

    changed variables names and inline styles

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,33 +1,29 @@
-const carousel = document.querySelector(".carousel");
-const slider = carousel.querySelector(".slider");
-const dotsContainer = carousel.querySelector(".dots-list");
-const dots = Array.from(dotsContainer.children);
-const numberOfPages = dots.length
-console.log(numberOfPages)
+const $carousel = document.querySelector(".carousel");
+const $slider = $carousel.querySelector(".slider");
+const $dotsContainer = $carousel.querySelector(".dots-list");
+const $dots = Array.from($dotsContainer.children);
+const numberOfPages = $dots.length
 
 let currentIndex = 0;
 
 
 // auto-play carousel logic
 setInterval(() => {
-  dots[currentIndex].classList.remove("active");
+  $dots[currentIndex].classList.remove("active");
   currentIndex = currentIndex === numberOfPages - 1 ? 0 : currentIndex + 1;
-  slider.style.transform = `translateX(${-100 * currentIndex}%)`;
-  slider.style.transition = ".7s";
-  dots[currentIndex].classList.add("active");
+  $slider.style.transform = `translateX(${-100 * currentIndex}%)`;
+  $dots[currentIndex].classList.add("active");
 }, 8000);
 
 // dots navigation carousel logic
 const onClickDot = (event) => {
-  dots[currentIndex].classList.remove("active");
+  $dots[currentIndex].classList.remove("active");
   const targetDot = event.target;
-  const targetIndex = dots.findIndex((dot) => dot === targetDot);
+  const targetIndex = $dots.findIndex((dot) => dot === targetDot);
   currentIndex = targetIndex;
-  slider.style.transform = `translateX(${-100 * currentIndex}%)`;
-  slider.style.transition = ".7s";
-  dots[currentIndex].classList.add("active");
+  $slider.style.transform = `translateX(${-100 * currentIndex}%)`;
+  $dots[currentIndex].classList.add("active");
 };
 
-for (let i = 0; i < dots.length; i++) {
-  dots[i].addEventListener("click", onClickDot);
-}
+
+$dots.forEach($dots => $dots.addEventListener("click", onClickDot))
