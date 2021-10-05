68f8ca296477e2af11d1ee404a9953a013d2fdb0
Melisa Izaquita <meliizaquita@gmail.com>
2021-10-05 18:25:19 -0500

    hero-temp.html is removed and in scss carousel

--- create-env.js
+++ create-env.js
@@ -1,9 +1,2 @@
-const fs = require('fs');
 
-fs.copyFile('.env.example', '.env', (err) => {
-  if (err) throw err;
-  console.log('.env was created successfully');
-});
-const Handlebars = require("handlebars");
-const template = Handlebars.compile("Name: {{name}}");
-console.log(template({ name: "Nils" }));
+
