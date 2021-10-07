87e4b9c572fcd58aabfb0056ae8f964573820e59
sofiaperezm <sofi.prz.m@gmail.com>
2021-10-07 17:20:24 -0500

    added a missing semicolon

--- src/js/letsLearnCategories.js
+++ src/js/letsLearnCategories.js
@@ -28,7 +28,6 @@ export const initLetsLearnCategories = () => {
         containerSidebar.appendChild(sidebarItem)
         containerDropdown.appendChild(optionItem)
     });
-}
 
     containerDropdown.addEventListener("change", selectedDropdownItem);
 }
